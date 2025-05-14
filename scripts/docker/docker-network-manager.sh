486a787c98a78ead35e5bfd7868ce6c68dca3436
hskim <59862781+hank-128bit@users.noreply.github.com>
2025-05-15 00:29:15 +0900

    feat: docker 스크립트 추가 (#1)

---
+++ scripts/docker/docker-network-manager.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+NETWORK_NAME="event-system-network"
+
+if [ "$1" == "create" ]; then
+    if [ ! "$(docker network ls | grep "$NETWORK_NAME")" ]; then
+        echo "Creating "$NETWORK_NAME""
+        docker network create "$NETWORK_NAME"
+    else
+        echo ""$NETWORK_NAME" already exists"
+    fi
+elif [ "$1" == "remove" ]; then
+    if [ ! "$(docker network ls | grep "$NETWORK_NAME")" ]; then
+        echo ""$NETWORK_NAME" does not exist"
+    else
+        echo "Removing "$NETWORK_NAME""
+        docker network rm "$NETWORK_NAME"
+    fi
+else
+    echo "Invalid argument"
+fi
