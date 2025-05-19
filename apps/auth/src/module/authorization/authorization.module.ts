5e563cdb714e3d084441ec16f735f1c298b3eeb8
hskim <59862781+hank-128bit@users.noreply.github.com>
2025-05-19 15:28:44 +0900

    feat(auth): 컨트롤러 함수 필터, 에러 적용

--- apps/auth/src/module/authorization/authorization.module.ts
+++ apps/auth/src/module/authorization/authorization.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AuthorizationController } from './authorization.controller';
+import { AuthorizationService } from './authorization.service';
+import { AdminModelModule } from '../database/model/admin/admin.model.module';
 
 @Module({
-  imports: [],
+  imports: [AdminModelModule],
   controllers: [AuthorizationController],
-  providers: [],
+  providers: [AuthorizationService],
+  exports: [AuthorizationService],
 })
 export class AuthorizationModule {}
---
+++ apps/auth/src/module/authorization/authorization.service.ts
@@ -0,0 +1,32 @@
+import { HttpStatus, Injectable } from '@nestjs/common';
+import { AdminRegRequestDto } from '@libs/interfaces/auth/auth.dto';
+import { AdminRoleMap } from '@libs/constants/admin.role';
+import { AdminModelService } from '../database/model/admin/admin.model.service';
+import { AuthServiceError } from '@libs/interfaces/error/auth_service.error';
+
+@Injectable()
+export class AuthorizationService {
+  constructor(private readonly adminModelService: AdminModelService) {}
+
+  async signin() {
+    return;
+  }
+
+  async signup(param: AdminRegRequestDto) {
+    const admin = await this.adminModelService.findByEmail(param.email);
+    if (admin) {
+      throw new AuthServiceError(
+        '이미 존재하는 이메일입니다',
+        HttpStatus.FORBIDDEN
+      );
+    }
+    const dto = await this.adminModelService.create({
+      email: param.email,
+      password: param.password,
+      name: param.name,
+      role: AdminRoleMap.NONE,
+    });
+    console.log(dto);
+    return dto;
+  }
+}
